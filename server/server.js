419c58790ab1c23d6a68afd43728b8fb017ac114
Dheeraj Chaudhary <Dheeraj.Chaudhary@contractor.hallmark.com>
2018-03-01 03:57:21 -0600

    Prod:build Yarn webpack

---
+++ server/server.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: Dheeraj Chaudhary 
+ * @Date: 2018-03-01 03:41:29 
+ * @Last Modified by: Dheeraj.Chaudhary@contractor.hallmark.com
+ * @Last Modified time: 2018-03-01 03:55:09
+ */
+const express = require('express');
+const path = require('path');
+const env = require('./config/config');
+
+//App to bind with public
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+//Middleware binding to express
+app.use(express.static(publicPath));
+
+//Router
+app.get('*', (req, res) => {
+  res.sendfile(path.join(publicPath, 'index.html'));
+});
+
+//APP server listener
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`ReactApp is running on  ${port} !!! `);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: Dheeraj Chaudhary 
  * @Date: 2018-02-28 21:52:44 
  * @Last Modified by: Dheeraj.Chaudhary@contractor.hallmark.com
- * @Last Modified time: 2018-03-01 03:06:46
+ * @Last Modified time: 2018-03-01 03:31:31
  */
 
 //Entry Point && Output Final bundle file- where to put
@@ -10,28 +10,31 @@ const path = require('path');
 const actualPath = path.join(__dirname, 'public');
 
 //Module Exports to webpack for bundling
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: actualPath
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: [/node_modules/, /.vscode/]
-      },
-      {
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        test: /\.s?css$/
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: actualPath
-  },
-  performance: { hints: false }
+module.exports = env => {
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    output: {
+      filename: 'bundle.js',
+      path: actualPath
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: [/node_modules/, /.vscode/]
+        },
+        {
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+          test: /\.s?css$/
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: actualPath
+    },
+    performance: { hints: false }
+  };
 };
